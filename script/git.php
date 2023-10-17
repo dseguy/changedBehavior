da4f69e5fb20f1e83da7d5a2e67c491ec6ab20e1
Damien Seguy <dseguy@exakat.io>
2023-10-17 11:04:05 +0200

    script to commit files of a CC together

---
+++ script/git.php
@@ -0,0 +1,25 @@
+<?php
+
+$list = $argv;
+array_shift($list);
+
+$total = 0;
+foreach($list as $element) {
+	if (!file_exists('codes/'.$element.'.php')) {
+		print "No such CC as $element in codes\n. Omitting.\n";
+		continue; 
+	}
+	
+	++$total;
+	
+	shell_exec('git stage codes/'.$element.'.php');
+	shell_exec('git stage docs/'.$element.'.json');
+	shell_exec('git stage results/*/'.$element.'.txt');
+}
+
+print "Found $total elements in the list of ".count($list)." provided\n";
+
+$count = trim(shell_exec('git status | grep "new file" | wc -l'));
+print "$count elements in stage\n";
+print "git commit -m 'Updated description of ".implode(', ', $list)."' \n";
+?>
