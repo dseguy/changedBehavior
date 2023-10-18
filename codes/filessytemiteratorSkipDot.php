ab6b966c3fef5aac84d002af06711db9c51d53e7
Damien Seguy <dseguy@exakat.io>
2023-10-18 10:10:56 +0200

    Updated description of filessytemiteratorSkipDot

--- codes/filessytemiteratorSkipDot.php
+++ codes/filessytemiteratorSkipDot.php
@@ -2,13 +2,15 @@
 
 mkdir('/tmp/fsisd', 0777);
 file_put_contents('/tmp/fsisd/a.txt', 'a');
+file_put_contents('/tmp/fsisd/.b', 'b');
 
-$it = new FilesystemIterator(dirname(__FILE__), FilesystemIterator::CURRENT_AS_FILEINFO);
+$it = new FilesystemIterator(dirname('/tmp/fsisd/a.txt'), FilesystemIterator::CURRENT_AS_FILEINFO);
 foreach ($it as $fileinfo) {
     echo $fileinfo->getFilename() . "\n";
 }
 
 unlink('/tmp/fsisd/a.txt');
+unlink('/tmp/fsisd/.b');
 rmdir('/tmp/fsisd');
 
 ?>
