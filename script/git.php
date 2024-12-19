54e270d13d282eb05510a3db676de6d475165d27
Damien Seguy <dseguy@exakat.io>
2024-12-19 12:19:20 +0100

    upgraded git.php

--- script/git.php
+++ script/git.php
@@ -5,6 +5,10 @@ array_shift($list);
 
 $total = 0;
 foreach($list as $element) {
+    if (substr($element, -4) === ".ini") {
+        $element = substr($element, 0, -4);
+    }
+
 	if (!file_exists('codes/'.$element.'.php')) {
 		print "No such CC as $element in codes\n. Omitting.\n";
 		continue; 
