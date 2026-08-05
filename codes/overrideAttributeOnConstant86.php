6a6c48cd2ac77cb04a95fee19a516c2fc7a5c075
Damien Seguy <dseguy@exakat.io>
2026-08-05 18:10:46 +0200

    Updated description of overrideAttributeOnConstant86

---
+++ codes/overrideAttributeOnConstant86.php
@@ -0,0 +1,15 @@
+<?php
+
+interface Shape {
+    const SIDES = 0;
+}
+
+class Square implements Shape {
+    #[\Override]
+    const SIDES = 4; // Fine, overrides Shape::SIDES
+
+    #[\Override]
+    const COLOR = 'blue'; // Fatal error, no matching parent constant
+}
+
+?>
