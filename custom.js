243ab1ed91afad5602caf265b2fc9869b1c54610
Damien Seguy <dseguy@exakat.io>
2026-08-20 13:15:55 +0200

    Add missing web files

---
+++ custom.js
@@ -0,0 +1,11 @@
+(function () {
+  var s = document.createElement('script');
+  s.async = true;
+  s.src = 'https://www.googletagmanager.com/gtag/js?id=G-29LCZ34CDW';
+  document.head.appendChild(s);
+})();
+
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+gtag('config', 'G-29LCZ34CDW');
